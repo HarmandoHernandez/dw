92c57ea7d45d9179b78e3037f7c93ccbbb54f47e
HarmandoHernandez <39201744+HarmandoHernandez@users.noreply.github.com>
2020-10-08 11:38:23 -0500

    Post Swipeable

---
+++ DiscoverWorld/app/src/main/java/com/utng/discoverw/AddPActivity.kt
@@ -0,0 +1,11 @@
+package com.utng.discoverw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_p)
+    }
+}
--- DiscoverWorld/app/src/main/java/com/utng/discoverw/HomeActivity.kt
+++ DiscoverWorld/app/src/main/java/com/utng/discoverw/HomeActivity.kt
@@ -1,8 +1,10 @@
 package com.utng.discoverw
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import com.google.firebase.auth.FirebaseAuth
 
@@ -12,9 +14,16 @@ enum class Providertype {
 }
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+        }
 
         // Setup
         val bundle = intent.extras
